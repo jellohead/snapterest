a9e5ac705a83087f7c361123212895898fe3abca
jellohead <jellohead7000@gmail.com>
2015-11-15 10:00:30 -1000

    Chapter 6 complete.

--- source/components/StreamTweet.react.js
+++ source/components/StreamTweet.react.js
@@ -60,6 +60,14 @@ var StreamTweet = React.createClass({
 
     return (nextProps.tweet.text.length > 1);
   },
+  componentWillUpdate: function(nextProps, nextState) {
+    console.log('[Snapterest] StreamTweet: 6. Running componentWillUpdate()');
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log('[Snapterest] StreamTweet: 7. Running componentDidUpdate()');
+
+    window.snapterest.numberOfDisplayedTweets++;
+  },
   componentWillUnmount: function() {
     console.log('[Snapterest] StreamTweet: 8. Running componentWillUnmount()');
     delete window.snapterest;
